d5bc2dc4d7a659a9b8800168b2b49d7d109e6713
Kovshik387 <fantokin03@mail.ru>
2024-07-02 18:19:42 +0300

    feat: добавил поле в proto-контракт

--- Systems/Api/Exchange.ExchangeValue/Services/ValuteService.cs
+++ Systems/Api/Exchange.ExchangeValue/Services/ValuteService.cs
@@ -9,14 +9,20 @@ using Exchange.Services.ValutaRate.Data.DTO;
 
 namespace Exchange.ExchangeValute.Services;
 
-public class ValuteService(IVoluteRateService voluteRateService, IMapper mapper, ILogger<ValuteService> logger) : Volute.VoluteBase
+public class ValuteService : Volute.VoluteBase
 {
-    private readonly IVoluteRateService _voluteRateService = voluteRateService;
-    private readonly IMapper _mapper = mapper;
-    private readonly ILogger _logger = logger;
+    private readonly IVoluteRateService _voluteRateService;
+    private readonly IMapper _mapper;
+    private readonly ILogger _logger;
+    private readonly string _locale = "ru-RU";
+    private readonly string _dateStringFormat = "dd.MM.yyyy";
+    public ValuteService(IVoluteRateService voluteRateService, IMapper mapper, ILogger<ValuteService> logger)
+    {
+        this._mapper = mapper; this._voluteRateService = voluteRateService; this._logger = logger;
+    }
     public override async Task<DailyValuteResponse> GetCurrentValue(DailyValuteRequest request, ServerCallContext context)
     {
-        if (DateOnly.TryParseExact(request.Date, "dd.MM.yyyy", CultureInfo.GetCultureInfo("ru-RU"), DateTimeStyles.None, out DateOnly parsedDate))
+        if (DateOnly.TryParseExact(request.Date, _dateStringFormat, CultureInfo.GetCultureInfo(_locale), DateTimeStyles.None, out DateOnly parsedDate))
         {
             _logger.LogInformation($"Date: {request.Date}|\tParsed Date: {parsedDate}");
             return _mapper.Map<DailyValuteResponse>(await _voluteRateService.GetCursByDateAsync(parsedDate));
@@ -26,16 +32,15 @@ public class ValuteService(IVoluteRateService voluteRateService, IMapper mapper,
 
     public override async Task<DynamicValueResponse> GetDynamicValue(DynamicValueRequest request, ServerCallContext context)
     {
-        DateOnly.TryParseExact(request.Date1, "dd.MM.yyyy", CultureInfo.GetCultureInfo("ru-RU"), DateTimeStyles.None, out DateOnly parsedDate1);
-        DateOnly.TryParseExact(request.Date2, "dd.MM.yyyy", CultureInfo.GetCultureInfo("ru-RU"), DateTimeStyles.None, out DateOnly parsedDate2);
+        DateOnly.TryParseExact(request.Date1, _dateStringFormat, CultureInfo.GetCultureInfo(_locale), DateTimeStyles.None, out DateOnly parsedDate1);
+        DateOnly.TryParseExact(request.Date2, _dateStringFormat, CultureInfo.GetCultureInfo(_locale), DateTimeStyles.None, out DateOnly parsedDate2);
         
         var records = await _voluteRateService.GetCursListByDateAsync(parsedDate1, parsedDate2, request.Name);
         
         var response = new DynamicValueResponse();
         response.Record.AddRange(_mapper.Map<List<RecordResponse>>(records));
-
         if (response.Record.Count.Equals(0)) throw new RpcException(new Status(StatusCode.NotFound, "За заданную дату нет данных"));
-
+        _logger.LogInformation($"name: {response.Record[0].Name}");
         return response;
     }
 }
