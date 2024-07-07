fb8a2179c69d0b34a2ed027107e63671195f74be
Kovshik387 <fantokin03@mail.ru>
2024-07-08 01:54:13 +0300

    feat: add worker for rabbitmq

---
+++ Systems/Worker/Exchange.Worker/Bootstrapper.cs
@@ -0,0 +1,17 @@
+﻿using Exchange.Services.Logger;
+using Exchange.Services.RabbitMq;
+using Exchange.Worker.TaskExecutor;
+
+namespace Exchange.Worker;
+
+public static class Bootstrapper
+{
+    public static IServiceCollection RegisterAppServices(this IServiceCollection services)
+    {
+        services
+            .AddAppLogger()
+            .AddRabbitMq()
+            ;
+        return services;
+    }
+}
---
+++ Systems/Worker/Exchange.Worker/TaskExecutor/TaskExecutor.cs
@@ -0,0 +1,36 @@
+﻿using Exchange.Services.EmailAction.Data;
+using Exchange.Services.Logger.Logger;
+using Exchange.Services.RabbitMq.Infrastructure;
+using System.Text;
+using System.Threading.Channels;
+
+namespace Exchange.Worker.TaskExecutor;
+
+public class TaskExecutor : BackgroundService
+{
+    private readonly IAppLogger logger;
+    private readonly IRabbitMq rabbitMq;
+    public TaskExecutor(
+        IAppLogger logger,
+        IRabbitMq rabbitMq
+    )
+    {
+        this.logger = logger;
+        this.rabbitMq = rabbitMq;
+    }
+
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        while (!stoppingToken.IsCancellationRequested)
+        {
+            rabbitMq.Subscribe<EmailModel>(RabbitQueue.PUBLICATE_ACCOUNT, async data =>
+            {
+                logger.Information($"email send: {data.accountModel.Email}");
+                foreach(var item in data.voluteModel)
+                {
+                    logger.Information($"{item.Name}: {item.Value}");
+                }
+            });
+        }
+    }
+}
