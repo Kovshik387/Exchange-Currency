44c62d36c09d58a8fdc0ed0b084fae3da7759111
Kovshik387 <fantokin03@mail.ru>
2024-07-09 21:18:49 +0300

    feat: деплой

--- Systems/Api/Exchange.ExchangeCache/Controllers/CacheController.cs
+++ Systems/Api/Exchange.ExchangeCache/Controllers/CacheController.cs
@@ -4,13 +4,13 @@ using Microsoft.AspNetCore.Mvc;
 namespace Exchange.ExchangeCache.Controllers
 {
     [ApiController]
-    [Route("api")]
+    [Route("api/exchange")]
     public class CacheController : ControllerBase
     {
         private readonly ICacheService _cacheService;
         public CacheController(ICacheService cacheService) => _cacheService = cacheService;
         
-        [HttpGet("/exchange-rates")]
+        [HttpGet("exchange-rates")]
         public async Task<IActionResult> GetData([FromQuery] string date)
         {
             if (DateOnly.TryParseExact(date, "dd.MM.yyyy",out _))
@@ -23,7 +23,7 @@ namespace Exchange.ExchangeCache.Controllers
             }
         }
 
-        [HttpGet("/exchange-rate")]
+        [HttpGet("exchange-rate")]
         public async Task<IActionResult> GetDynamicValue([FromQuery] string date1, [FromQuery] string date2, [FromQuery] string name)
         {
             if (DateOnly.TryParseExact(date1, "dd.MM.yyyy", out _) && DateOnly.TryParseExact(date2, "dd.MM.yyyy", out _))
--- Systems/Web/exchange-rates/src/utils/routers.ts
+++ Systems/Web/exchange-rates/src/utils/routers.ts
@@ -2,8 +2,8 @@ import defaultUri from "./defaultRoute";
 
 
 export const Routes = {
-    ExchangeRate: `${defaultUri}8020/exchange-rate`,
-    ExchangeRates: `${defaultUri}8020/exchange-rates`
+    ExchangeRate: `${defaultUri}8020/api/exchange/exchange-rate`,
+    ExchangeRates: `${defaultUri}8020/api/exchange/exchange-rates`
 }
 
 export default Routes;
