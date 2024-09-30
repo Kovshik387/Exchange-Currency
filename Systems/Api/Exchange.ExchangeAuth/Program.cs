a93abafc2945c372078c6858568d3124db947db0
Kovshik387 <fantokin03@mail.ru>
2024-09-30 16:12:13 +0300

    feat: add dependencies

--- Systems/Api/Exchange.ExchangeAuth/Program.cs
+++ Systems/Api/Exchange.ExchangeAuth/Program.cs
@@ -1,14 +1,32 @@
 using Exchange.Authorization.Context;
 using Exchange.Authorization.Context.Setup;
 using Exchange.ExchangeAuth.Configuration;
+using Exchange.Common.Settings;
+using Exchange.Services.Settings.SettingsConfigure;
+using Exchange.ExchangeAuth;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+var mainSettings = Settings.Load<MainSettings>("Main");
+var logSettings = Settings.Load<LogSettings>("Log");
+var swaggerSettings = Settings.Load<SwaggerSettings>("Swagger");
+var authSettings = Settings.Load<AuthSettings>("Auth");
+
+builder.AddAppLogger(mainSettings, logSettings);
+
+builder.Services.AddAppCors();
 builder.Services.AddAppDbContext(builder.Configuration);
+builder.Services.AddControllers();
+builder.Services.AddGrpc();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddAppAuth(authSettings);
+builder.Services.AddAppSwagger(mainSettings, swaggerSettings, authSettings);
+builder.Services.RegisterService();
+builder.Services.AddAppAutoMappers();
+builder.Services.AddAppValidator();
 
 var app = builder.Build();
+
 app.UseAppCors();
 app.UseAppSwagger();
 app.UseAppAuth();
